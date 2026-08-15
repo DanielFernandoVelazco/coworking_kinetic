5b6787083ae3299378e6070033ec5aafd23ff51f
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-08-14 21:26:12 -0500

    frontend/src/App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,25 +1,28 @@
-// src/App.jsx
-import React from 'react'
-import { AuthProvider } from './context/AuthContext'
-import AppRoutes from './AppRoutes'
-import { Toaster } from 'react-hot-toast'
+// frontend/src/App.jsx
+import React from 'react';
+import { AuthProvider } from './context/AuthContext';
+import { CartProvider } from './context/CartContext';
+import AppRoutes from './AppRoutes';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <AuthProvider>
-      <AppRoutes />
-      <Toaster
-        position="top-right"
-        toastOptions={{
-          duration: 4000,
-          style: {
-            background: '#363636',
-            color: '#fff',
-          },
-        }}
-      />
+      <CartProvider>
+        <AppRoutes />
+        <Toaster
+          position="top-right"
+          toastOptions={{
+            duration: 4000,
+            style: {
+              background: '#363636',
+              color: '#fff',
+            },
+          }}
+        />
+      </CartProvider>
     </AuthProvider>
-  )
+  );
 }
 
-export default App
+export default App;
