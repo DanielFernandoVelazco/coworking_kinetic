9f1538622f93b348949b061b50f1cbee00a314bc
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-08-18 21:02:27 -0500

    frontend/src/components/layout/MainLayout.jsx

--- frontend/src/components/layout/MainLayout.jsx
+++ frontend/src/components/layout/MainLayout.jsx
@@ -1,3 +1,4 @@
+// frontend/src/components/layout/MainLayout.jsx
 import React from 'react';
 import { Outlet } from 'react-router-dom';
 import Navbar from '../common/Navbar';
@@ -5,7 +6,7 @@ import Footer from '../common/Footer';
 
 const MainLayout = () => {
     return (
-        <div className="min-h-screen flex flex-col bg-background">
+        <div className="min-h-screen flex flex-col bg-background dark:bg-dark-background transition-colors duration-300">
             <Navbar />
             <main className="flex-grow">
                 <Outlet />
