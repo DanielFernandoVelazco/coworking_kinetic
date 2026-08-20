e74e92717f970eea00fd8d7db2027d91f4b9f7a4
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-08-19 21:46:46 -0500

    frontend/src/App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,28 +3,30 @@ import React from 'react';
 import { AuthProvider } from './context/AuthContext';
 import { CartProvider } from './context/CartContext';
 import { ThemeProvider } from './context/ThemeContext';
+import { AlertProvider } from './context/AlertContext';
 import AppRoutes from './AppRoutes';
 import { Toaster } from 'react-hot-toast';
 import ThemeToggle from './components/common/ThemeToggle';
 
 function App() {
   return (
-    // ✅ ThemeProvider debe ser el más externo
     <ThemeProvider>
       <AuthProvider>
         <CartProvider>
-          <AppRoutes />
-          <Toaster
-            position="top-right"
-            toastOptions={{
-              duration: 4000,
-              style: {
-                background: '#363636',
-                color: '#fff',
-              },
-            }}
-          />
-          <ThemeToggle />
+          <AlertProvider>
+            <AppRoutes />
+            <Toaster
+              position="top-right"
+              toastOptions={{
+                duration: 4000,
+                style: {
+                  background: '#363636',
+                  color: '#fff',
+                },
+              }}
+            />
+            <ThemeToggle />
+          </AlertProvider>
         </CartProvider>
       </AuthProvider>
     </ThemeProvider>
