20f22726c880dfd67be3283a93e218cf42a854ae
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-09-07 19:50:10 -0500

    Usings.cs

---
+++ backend/KineticWorkspace.API.Tests/Usings.cs
@@ -0,0 +1,11 @@
+// KineticWorkspace.API.Tests/Usings.cs
+global using Xunit;
+global using Moq;
+global using FluentAssertions;
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.Extensions.Logging;
+global using AutoMapper;
+global using System;
+global using System.Collections.Generic;
+global using System.Linq;
+global using System.Threading.Tasks;
