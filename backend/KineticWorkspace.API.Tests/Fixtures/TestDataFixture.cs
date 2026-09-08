cfad54ece73c3bda247512cd9a1ec3a5985811fd
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-09-07 19:50:46 -0500

    Fixtures/TestDataFixture.cs

---
+++ backend/KineticWorkspace.API.Tests/Fixtures/TestDataFixture.cs
@@ -0,0 +1,11 @@
+namespace KineticWorkspace.API.Tests.Fixtures;
+
+public class TestDataFixture
+{
+    public User TestUser => TestDataFactory.CreateTestUser();
+    public Space TestSpace => TestDataFactory.CreateTestSpace();
+    public Reservation TestReservation => TestDataFactory.CreateTestReservation();
+    public Alert TestAlert => TestDataFactory.CreateTestAlert();
+    public Amenity TestAmenity => TestDataFactory.CreateTestAmenity();
+    public PreReservation TestPreReservation => TestDataFactory.CreateTestPreReservation();
+}
