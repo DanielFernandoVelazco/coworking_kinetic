87120caf9dda8236d80bd70a3286f6e7de6aa248
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-09-07 20:01:59 -0500

    Usings.cs

--- backend/KineticWorkspace.API.Tests/Usings.cs
+++ backend/KineticWorkspace.API.Tests/Usings.cs
@@ -9,3 +9,13 @@ global using System;
 global using System.Collections.Generic;
 global using System.Linq;
 global using System.Threading.Tasks;
+
+// ✅ AGREGAR REFERENCIAS A LAS ENTIDADES DEL PROYECTO PRINCIPAL
+global using KineticWorkspace.API.Models.Entities;
+global using KineticWorkspace.API.Models.DTOs.Alerts;
+global using KineticWorkspace.API.Models.DTOs.Auth;
+global using KineticWorkspace.API.Models.DTOs.Reservations;
+global using KineticWorkspace.API.Models.DTOs.Spaces;
+global using KineticWorkspace.API.Models.DTOs.Admin;
+global using KineticWorkspace.API.Models.DTOs.Amenities;
+global using KineticWorkspace.API.Helpers;
