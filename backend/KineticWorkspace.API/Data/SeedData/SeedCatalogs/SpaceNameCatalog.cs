0aa2b3d1ddac7cc9aeaf6a4ed072c22c7293df1b
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-09-11 20:38:55 -0500

    KineticWorkspace.API/Data/SeedData/SeedCatalogs/SpaceNameCatalog.cs

---
+++ backend/KineticWorkspace.API/Data/SeedData/SeedCatalogs/SpaceNameCatalog.cs
@@ -0,0 +1,12 @@
+namespace KineticWorkspace.API.Data.SeedData.SeedCatalogs
+{
+    public static class SpaceNameCatalog
+    {
+        public static readonly List<string> Names = new()
+        {
+            "Skyline", "Zen", "Apex", "Vista", "Nova",
+            "Eclipse", "Pulse", "Summit", "Orbit", "Haven",
+            "Valor", "Aura", "Flux", "Vertex", "Apex"
+        };
+    }
+}
