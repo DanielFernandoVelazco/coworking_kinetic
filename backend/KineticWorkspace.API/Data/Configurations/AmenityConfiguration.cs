24b139327ba7339602f2c6651542640e844a29a7
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-09-11 20:33:09 -0500

    KineticWorkspace.API/Data/Configurations/AmenityConfiguration.cs

---
+++ backend/KineticWorkspace.API/Data/Configurations/AmenityConfiguration.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using KineticWorkspace.API.Models.Entities;
+
+namespace KineticWorkspace.API.Data.Configurations
+{
+    public class AmenityConfiguration : IEntityTypeConfiguration<Amenity>
+    {
+        public void Configure(EntityTypeBuilder<Amenity> builder)
+        {
+            builder.ToTable("Amenities");
+        }
+    }
+}
