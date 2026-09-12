751a513b279969986d2a0b61907cb1b84b75538d
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-09-11 20:34:16 -0500

    KineticWorkspace.API/Data/Configurations/PasswordResetTokenConfiguration.cs

---
+++ backend/KineticWorkspace.API/Data/Configurations/PasswordResetTokenConfiguration.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using KineticWorkspace.API.Models.Entities;
+
+namespace KineticWorkspace.API.Data.Configurations
+{
+    public class PasswordResetTokenConfiguration : IEntityTypeConfiguration<PasswordResetToken>
+    {
+        public void Configure(EntityTypeBuilder<PasswordResetToken> builder)
+        {
+            builder.ToTable("PasswordResetTokens");
+        }
+    }
+}
