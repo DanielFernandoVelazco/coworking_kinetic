596637f80e3fa382d036082dc123c601b616ea52
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-09-11 20:35:18 -0500

    KineticWorkspace.API/Data/Configurations/RefreshTokenConfiguration.cs

---
+++ backend/KineticWorkspace.API/Data/Configurations/RefreshTokenConfiguration.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using KineticWorkspace.API.Models.Entities;
+
+namespace KineticWorkspace.API.Data.Configurations
+{
+    public class RefreshTokenConfiguration : IEntityTypeConfiguration<RefreshToken>
+    {
+        public void Configure(EntityTypeBuilder<RefreshToken> builder)
+        {
+            builder.ToTable("RefreshTokens");
+        }
+    }
+}
