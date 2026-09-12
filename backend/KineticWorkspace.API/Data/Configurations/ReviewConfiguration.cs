bb478315c2114bcf8be6bd0918590dbfbbf70bbd
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-09-11 20:35:59 -0500

    KineticWorkspace.API/Data/Configurations/ReviewConfiguration.cs

---
+++ backend/KineticWorkspace.API/Data/Configurations/ReviewConfiguration.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using KineticWorkspace.API.Models.Entities;
+
+namespace KineticWorkspace.API.Data.Configurations
+{
+    public class ReviewConfiguration : IEntityTypeConfiguration<Review>
+    {
+        public void Configure(EntityTypeBuilder<Review> builder)
+        {
+            builder.ToTable("Reviews");
+        }
+    }
+}
