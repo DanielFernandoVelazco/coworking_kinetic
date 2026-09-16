d7e94fcdf73ffcfce86c4f6cdc3b588ba97afebe
FernandoVelazco <daniel.fernando.velazco@gmail.com>
2026-09-15 21:53:59 -0500

    src/api/config/tokenStorage.js

---
+++ frontend/src/api/config/tokenStorage.js
@@ -0,0 +1,25 @@
+// frontend/src/api/config/tokenStorage.js
+
+const ACCESS_TOKEN_KEY = 'accessToken';
+const REFRESH_TOKEN_KEY = 'refreshToken';
+const USER_KEY = 'user';
+
+export const tokenStorage = {
+    getAccessToken: () => localStorage.getItem(ACCESS_TOKEN_KEY),
+    getRefreshToken: () => localStorage.getItem(REFRESH_TOKEN_KEY),
+
+    setTokens: (accessToken, refreshToken) => {
+        localStorage.setItem(ACCESS_TOKEN_KEY, accessToken);
+        if (refreshToken) {
+            localStorage.setItem(REFRESH_TOKEN_KEY, refreshToken);
+        }
+    },
+
+    clear: () => {
+        localStorage.removeItem(ACCESS_TOKEN_KEY);
+        localStorage.removeItem(REFRESH_TOKEN_KEY);
+        localStorage.removeItem(USER_KEY);
+    },
+};
+
+export default tokenStorage;
